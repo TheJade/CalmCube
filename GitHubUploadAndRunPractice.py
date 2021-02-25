3e4d52d32488ba20e1fd7bf429f568fffac91187
Jade Fjestad <40924189+TheJade@users.noreply.github.com>
2021-02-25 12:53:21 -0700

    Added a practice file
    Added a practice file for uploading and running code on the raspberry pi

---
+++ GitHubUploadAndRunPractice.py
@@ -0,0 +1,16 @@
+#login as:    pi
+#password:    raspberry
+
+#upload to github this edited file to github
+#download onto the pi usingthe pi cmd:   git clone URL_FROM_GITHUB
+#note: you cannot past with cntl+v all you do is right click to past into the cmd
+#cmd to list the folder/files in current directory:     ls
+#cmd to change directory:       cd DIRECTORY_NAME
+#cmd to go back a directory:    cd ..
+#to run the python code use on the pi cmd:       python3 FILE_NAME.py
+#after you run the code you should see a print out of the below command
+#note: use cntl+c to quit a program in progress
+
+#edit the below to your own name
+
+print("Your name here")
