cb6d2ebaef45a776a00c05f415a1efab3591c85b
Jade Fjestad <40924189+TheJade@users.noreply.github.com>
2021-03-01 22:42:11 -0700

    Testing results added

--- Justin1.py
+++ Justin1.py
@@ -12,25 +12,25 @@ try:
         if ((1.1 - (time.time() - timestart)) <= 0.1 ):
             x += 1
             timestart = time.time()
-            if (x == 1):
-                spi.writebytes([0b10000000]) # write one byte
-                print("if 1 worked, red1")
-            elif (x == 2):
-                spi.writebytes([0b01000000]) # write one byte
-                print("if 2 worked, green1")
-            elif (x == 3):
-                spi.writebytes([0b00100000]) # write one byte
-                print("if 3 worked, blue1")
-            elif (x == 4):
-                spi.writebytes([0b00_01110]) # write one byte
-                print("if 4 worked, white2")
-            elif (x == 5):
-                spi.writebytes([0b00_00001]) # write one byte
-                print("if 5 worked, red2")
-            elif (x == 6):
-
-                spi.writebytes([0b01001001])
-                spi.writebytes([0b10010010]) # write one byte
+            if (x == 1):                                                                #test results:
+                spi.writebytes([0b10000000]) # write one byte                           #green3            
+                print("if 1 worked, red1")                  
+            elif (x == 2):                  
+                spi.writebytes([0b01000000]) # write one byte                           #red3
+                print("if 2 worked, green1")                    
+            elif (x == 3):                  
+                spi.writebytes([0b00100000]) # write one byte                           #red6
+                print("if 3 worked, blue1")                 
+            elif (x == 4):                  
+                spi.writebytes([0b00_01110]) # write one byte                           #red2
+                print("if 4 worked, white2")                    
+            elif (x == 5):                  
+                spi.writebytes([0b00_00001]) # write one byte                           #red1 red5
+                print("if 5 worked, red2")                  
+            elif (x == 6):                  
+                                
+                spi.writebytes([0b01001001])                    
+                spi.writebytes([0b10010010]) # write one byte                           #green4 green5 green6
                 spi.writebytes([0b00])
                 print("if 6 worked, all on blue")
             elif (x == 7):
--- Justin3.py
+++ Justin3.py
@@ -9,25 +9,25 @@ x = 0
 
 try:
     while True: # endless loop, press Ctrl+C to exit
-        time.sleep(1)
-        spi.writebytes([0b10000000]) # write one byte
-        print("if 1 worked, red1")
-        time.sleep(1)
-        spi.writebytes([0b01000000]) # write one byte
-        print("if 2 worked, green1")
-        time.sleep(1)
-        spi.writebytes([0b00100000]) # write one byte
-        print("if 3 worked, blue1")
-        time.sleep(1)
-        spi.writebytes([0b00_01110]) # write one byte
-        print("if 4 worked, white2")
-        time.sleep(1)
-        spi.writebytes([0b00_00001]) # write one byte
-        print("if 5 worked, red2")
-        time.sleep(1)
-        spi.writebytes([0b01001001])
-        spi.writebytes([0b10010010]) # write one byte
-        spi.writebytes([0b00])
+        time.sleep(1)                                                   #test results:
+        spi.writebytes([0b10000000]) # write one byte                   #green3
+        print("if 1 worked, red1")          
+        time.sleep(1)           
+        spi.writebytes([0b01000000]) # write one byte                   #red3
+        print("if 2 worked, green1")            
+        time.sleep(1)           
+        spi.writebytes([0b00100000]) # write one byte                   #red6
+        print("if 3 worked, blue1")         
+        time.sleep(1)           
+        spi.writebytes([0b00_01110]) # write one byte                   #red2
+        print("if 4 worked, white2")            
+        time.sleep(1)           
+        spi.writebytes([0b00_00001]) # write one byte                   #red1 red5
+        print("if 5 worked, red2")          
+        time.sleep(1)           
+        spi.writebytes([0b01001001])            
+        spi.writebytes([0b10010010]) # write one byte                   #green4 green5 green6
+        spi.writebytes([0b00])          
         print("if 6 worked, all on blue")
         time.sleep(1)
         print("loop restart")
