3167da48187545cb1a0b09f663b4fc607b5ce400
JustinRoss911 <rossjustin911@gmail.com>
2021-03-30 20:12:39 -0600

    Update PythonCode4.py

--- PythonCode_3_30/PythonCode4.py
+++ PythonCode_3_30/PythonCode4.py
@@ -25,9 +25,9 @@ spi.max_speed_hz = 20000  #this class attribute defines the max speed the data w
 
 try:       #if an error occurs in the try then it will execute finally
     while True: #will loop forever
-        spi.writebytes([0b11111111])
-        spi.writebytes([0b11111111])
-        spi.writebytes([0b01000011]) #all LED's in row 2 should be white
+        spi.writebytes([0b11111111]) # every LED and ROW should be on
+        #spi.writebytes([0b11111111])
+        #spi.writebytes([0x01000011]) #all LED's in row 2 should be white
 
 finally:
 
